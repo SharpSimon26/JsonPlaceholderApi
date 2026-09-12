7f1e2c0c5a80322c3ed8cc24b554e22df30535f6
PySimon25 <213518354+PySimon25@users.noreply.github.com>
2026-09-12 15:53:00 +0200

    Nuovo test per endpoint Post

--- test/JsonPlaceholder.Api.Tests/Endpoints/v1/PostEndpointTests.cs
+++ test/JsonPlaceholder.Api.Tests/Endpoints/v1/PostEndpointTests.cs
@@ -27,6 +27,73 @@ public class PostEndpointTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Fact]
+    public async Task GetPosts_Returns200AndPosts()
+    {
+        // Arrange
+        var expectedPosts = new List<Post>
+        {
+            new()
+            {
+                Id = 5,
+                UserId = 8,
+                Title = "Test Post 1",
+                Body = "Test body 1"
+            },
+            new()
+            {
+                Id = 6,
+                UserId = 8,
+                Title = "Test Post 2",
+                Body = "Test body 2"
+            },
+            new()
+            {
+                Id = 7,
+                UserId = 10,
+                Title = "Test Post 3",
+                Body = "Test body 3"
+            },
+            new()
+            {
+                Id = 8,
+                UserId = 15,
+                Title = "Test Post 4",
+                Body = "Test body 4"
+            }
+        };
+
+        _mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(expectedPosts);
+
+        var client = _factory.CreateClient();        
+    
+        // Act
+        var response = await client.GetAsync("/api/v1/posts");
+    
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var posts = await response.Content.ReadFromJsonAsync<IEnumerable<Post>>();
+        Assert.IsType<IEnumerable<Post>>(posts, exactMatch: false);
+        Assert.Equal(4, posts.Count());
+        Assert.All(posts, p => {
+            Assert.True(p.Id > 0);
+            Assert.True(p.UserId > 0);
+            Assert.False(string.IsNullOrWhiteSpace(p.Title));
+            Assert.False(string.IsNullOrWhiteSpace(p.Body));
+        });
+        Assert.Equal(5, posts.First().Id);
+        Assert.Equal(8, posts.First().UserId);
+        Assert.Equal("Test Post 1", posts.First().Title);
+        Assert.Equal("Test body 1", posts.First().Body);
+        Assert.Equal(8, posts.Last().Id);
+        Assert.Equal(15, posts.Last().UserId);   
+        Assert.Equal("Test Post 4", posts.Last().Title);
+        Assert.Equal("Test body 4", posts.Last().Body);
+
+        // Verifica che il repo venga chiamato
+        _mockRepo.Verify(r => r.GetAllAsync(), Times.Once);
+    }
+
+    [Fact]
     public async Task GetPostById_WhenExists_Returns200AndPost()
     {
         // Arrange
